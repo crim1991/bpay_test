8d6617a53cbcd7a24880be6e956dcebda7ba8b49
crim1991 <cristighirba@gmail.com>
2022-03-24 17:36:48 +0200

    Added sanctum auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response()->json($response);
+    }
+
+    public function logout(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        $user->tokens()->delete();
+
+        $response = [
+            'message' => 'Logged out'
+        ];
+
+        return response()->json($response);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,10 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
 Route::prefix('order')->group(function () {
-    Route::resource('/', OrderController::class);
-    Route::get('show/{id}', [OrderController::class, 'show']);
-    Route::get('delivery-price', [OrderController::class, 'calculateDelivery']);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::resource('/', OrderController::class);
+        Route::get('show/{id}', [OrderController::class, 'show']);
+        Route::get('delivery-price', [OrderController::class, 'calculateDelivery']);
+    });
 });
 
 
